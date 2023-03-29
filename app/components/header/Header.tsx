e70ebf21719d2446c1157ea1632ed1590b42f29b
Dmytro <dmitrymusiichenko@gmail.com>
2023-03-29 16:13:08 +0300

    header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -6,48 +6,55 @@ import {
   MenuItem,
   MenuList,
 } from "@chakra-ui/react";
+import Image from "next/image";
 import { FC } from "react";
 import { FiMenu } from "react-icons/fi";
 
 const Header: FC = () => {
   return (
-    <div className="inline-block self-end">
-      <Menu>
-        <MenuButton
-          as={Button}
-          bg={"transparent"}
-          _hover={{ background: "transparent" }}
-          _active={{ background: "transparent" }}
-        >
-          <Icon as={FiMenu} boxSize={"10"} />
-        </MenuButton>
-        <MenuList borderRadius={"3xl"} overflow={"hidden"}>
-          <MenuItem
-            as={"a"}
-            href="#projects"
-            color="#F59E0B"
-            className="flex justify-center font-medium"
+    <div className="flex justify-between items-center">
+      <div className="px-4 flex gap-3 items-center">
+        <Image src="/icon-surname.svg" alt="logo" width={16} height={40} />
+        <span className="font-medium">Musiichenko</span>
+      </div>
+      <div className="inline-block">
+        <Menu>
+          <MenuButton
+            as={Button}
+            bg={"transparent"}
+            _hover={{ background: "transparent" }}
+            _active={{ background: "transparent" }}
           >
-            Projects
-          </MenuItem>
-          <MenuItem
-            as={"a"}
-            href="#about"
-            color="#38BDF8"
-            className="flex justify-center font-medium"
-          >
-            About me
-          </MenuItem>
-          <MenuItem
-            as={"a"}
-            href="#contacts"
-            color="#34D399"
-            className="flex justify-center font-medium"
-          >
-            Contacts
-          </MenuItem>
-        </MenuList>
-      </Menu>
+            <Icon as={FiMenu} boxSize={"10"} />
+          </MenuButton>
+          <MenuList borderRadius={"3xl"} overflow={"hidden"}>
+            <MenuItem
+              as={"a"}
+              href="#projects"
+              color="#F59E0B"
+              className="flex justify-center font-medium"
+            >
+              Projects
+            </MenuItem>
+            <MenuItem
+              as={"a"}
+              href="#about"
+              color="#38BDF8"
+              className="flex justify-center font-medium"
+            >
+              About me
+            </MenuItem>
+            <MenuItem
+              as={"a"}
+              href="#contacts"
+              color="#34D399"
+              className="flex justify-center font-medium"
+            >
+              Contacts
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
     </div>
   );
 };
