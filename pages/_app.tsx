5520eef98d8221712346fbea29bc90c6ba6387fe
Dmytro <dmitrymusiichenko@gmail.com>
2023-03-31 12:27:13 +0300

    remove theme from chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,10 @@
 import "@/styles/globals.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-
-const theme = extendTheme({
-  config: {
-    initialColorMode: "system",
-    useSystemColorMode: true,
-  },
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
   );
