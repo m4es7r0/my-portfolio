5373044e8df4f8f16a55d3b4cab7f953f02c16f4
Dmitry Musiichenko <41909013+m4es7r0@users.noreply.github.com>
2023-04-09 15:46:56 +0300

    upd: projects actions

--- app/components/project/Project.tsx
+++ app/components/project/Project.tsx
@@ -1,8 +1,9 @@
 import type { Project } from "@/pages/api/projects";
 
-import { Icon } from "@chakra-ui/react";
 import Image from "next/image";
 import { FC } from "react";
+
+import { Icon } from "@chakra-ui/react";
 import { BiGlobe } from "react-icons/bi";
 import { VscGithubInverted } from "react-icons/vsc";
 
@@ -19,31 +20,49 @@ const Project: FC<{ data: Project }> = ({ data }) => {
       >
         {data.name}
       </h3>
+
       <div className="flex gap-[10px] overflow-x-auto">
         {data.stack.map((skill) => (
           <SkillBadge data={skill} key={skill.name} />
         ))}
       </div>
-      <div className="flex justify-center items-center text-white px-4 text-3xl font-medium   min-h-[270px] bg-gray-900 rounded-3xl">
+
+      <a
+        href={`${data.deploy ?? data.git}`}
+        target="_blank"
+        className="flex justify-center items-center text-white p-6 text-3xl font-medium min-h-[270px] bg-gray-900 rounded-3xl transition-transform hover:scale-105"
+      >
         {data.image ? (
-          <Image
+          <img
             src={data.image}
             alt="project preview"
             width={1000}
             height={1000}
+            className="self-stretch rounded-2xl object-cover"
           />
         ) : (
           "coming soon..."
         )}
-      </div>
+      </a>
+
       <div className="flex gap-5 ml-4 font-medium">
         {data.git && (
-          <a href={data.git} target="_blank" title="Github">
+          <a
+            href={data.git}
+            target="_blank"
+            title="Github"
+            className="transition-transform hover:-translate-y-1"
+          >
             Github {<Icon as={VscGithubInverted} />}
           </a>
         )}
         {data.deploy && (
-          <a href={data.deploy} target="_blank" title="Deploy">
+          <a
+            href={data.deploy}
+            target="_blank"
+            title="Deploy"
+            className="transition-transform hover:-translate-y-1"
+          >
             Deploy {<Icon as={BiGlobe} />}
           </a>
         )}
